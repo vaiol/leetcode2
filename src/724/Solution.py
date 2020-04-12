10df4bf03217d9b1a9429baa7fd110e51d94ab77
Alexander Stepanov <vaiol.ans@gmail.com>
2020-04-12 13:56:59 +0300

    724 refactor

--- src/724/Solution.py
+++ src/724/Solution.py
@@ -4,14 +4,13 @@ from typing import List
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         total_sum = 0
-        computed_sum = [0] * len(nums)
-        for i, n in enumerate(nums):
-            computed_sum[i] = total_sum
+        for n in nums:
             total_sum += n
-        for i in range(len(nums)):
-            right_sum = total_sum - nums[i] - computed_sum[i]
-            if right_sum == computed_sum[i]:
+        left_sum = 0
+        for i, n in enumerate(nums):
+            if total_sum - left_sum - n == left_sum:
                 return i
+            left_sum += n
         return -1
 
 
