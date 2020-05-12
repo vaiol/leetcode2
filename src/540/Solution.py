12c3febd1d34ca0534292cc796bd997baf159fed
Alexander Stepanov <vaiol.ans@gmail.com>
2020-05-12 13:04:21 +0300

    540

---
+++ src/540/Solution.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        start, end = 0, len(nums) - 1
+        while start < end:
+            mid = (end + start) // 2
+            if mid % 2 == 1:
+                if nums[mid] == nums[mid - 1]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            if mid % 2 == 0:
+                if nums[mid] == nums[mid - 1]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+        return nums[start] if start % 2 == 0 else nums[start - 1]
+
+
