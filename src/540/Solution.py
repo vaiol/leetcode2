6803317a95d666f731759d80c41362e69e1c86fa
Alexander Stepanov <vaiol.ans@gmail.com>
2020-05-12 13:09:22 +0300

    540

--- src/540/Solution.py
+++ src/540/Solution.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def singleNonDuplicate(self, nums: List[int]) -> int:
+    def singleNonDuplicate_1(self, nums: List[int]) -> int:
         start, end = 0, len(nums) - 1
         while start < end:
             mid = (end + start) // 2
@@ -17,5 +17,17 @@ class Solution:
                 else:
                     start = mid + 1
         return nums[start] if start % 2 == 0 else nums[start - 1]
+
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        start, end = 0, len(nums) - 1
+        while start < end:
+            mid = (end + start) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] == nums[mid + 1]:
+                start = mid + 2
+            else:
+                end = mid - 1
+        return nums[start]
 
 
