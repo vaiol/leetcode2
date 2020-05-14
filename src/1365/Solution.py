05d15f37e17b71bf61fd307a4008a1ee8f554e3a
Alexander Stepanov <vaiol.ans@gmail.com>
2020-05-14 14:11:05 +0300

    1313

---
+++ src/1365/Solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        d = dict()
+        sorted_nums = sorted(nums)
+        for i, n in enumerate(sorted_nums):
+            if n not in d:
+                d[n] = i
+        return [d[n] for n in nums]
