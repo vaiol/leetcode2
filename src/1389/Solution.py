7e0fe0a18c1501a56e3e15d26d6015dd8bfcf29f
Alexander Stepanov <vaiol.ans@gmail.com>
2020-05-14 14:26:20 +0300

    1389

---
+++ src/1389/Solution.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        res = []
+        for n, i in zip(nums, index):
+            res.insert(i, n)
+        return res
