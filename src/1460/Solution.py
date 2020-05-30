70702c19b3184a31d440c908f40affaf7b99ebeb
Alexander Stepanov <vaiol.ans@gmail.com>
2020-05-30 19:47:35 +0300

    1460

---
+++ src/1460/Solution.py
@@ -0,0 +1,7 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        return Counter(target) == Counter(arr)
