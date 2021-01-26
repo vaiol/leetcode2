a50a320aac3151be2c46790ad66a4bb44eea3251
Alexander Stepanov <vaiol.ans@gmail.com>
2021-01-26 16:29:06 +0200

    941

---
+++ src/941/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        N = len(arr)
+        i = 0
+        while i < N - 1 and arr[i] < arr[i + 1]:
+            i += 1
+        if i == 0 or i == N - 1:
+            return False
+            
+        while i < N - 1 and arr[i] > arr[i + 1]:
+            i += 1
+        
+        return i == N - 1
