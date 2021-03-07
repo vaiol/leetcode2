67c06faaf3745e65346bc8ed279b6cd44f2caf8d
Alexander Stepanov <vaiol.ans@gmail.com>
2021-03-07 21:11:49 +0200

    820

---
+++ src/820/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+        s = set(words)
+        ss = set(s)
+        for w in ss:
+            for i in range(1, len(w)):
+                word_to_remove = w[i:]
+                if word_to_remove in s:
+                    s.remove(word_to_remove)
+        return sum(len(w) + 1 for w in s)
