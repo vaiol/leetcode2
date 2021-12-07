9bad842c3452f8a1128fffe56f48a425d8936637
Alexander Stepanov <vaiol.ans@gmail.com>
2021-12-07 11:59:22 +0200

    1290

---
+++ src/1290/Solution.py
@@ -0,0 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        curr = head
+        
+        res = 0
+        while curr:
+            res = res * 2 + curr.val
+            curr = curr.next
+            
+        return res
