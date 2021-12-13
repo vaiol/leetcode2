7a60a243b50b0ec45eea3aeb021eaa822abb0fa8
Alexander Stepanov <vaiol.ans@gmail.com>
2021-12-13 19:42:19 +0200

    1446

--- src/1446/Solution.py
+++ src/1446/Solution.py
@@ -13,3 +13,19 @@ class Solution:
             power = max(power, j)
             i += j
         return power
+
+    def maxPower1(self, s: str) -> int:
+        #13.12.2021
+        N = len(s)
+        if N <= 1:
+            return N
+
+        res = 1
+        curr_n = 1
+        for i in range(1, N):
+            if s[i] != s[i - 1]:
+                res = max(res, curr_n)
+                curr_n = 0
+            curr_n += 1
+        return max(res, curr_n)
+
