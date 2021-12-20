3455f73eae0357335f4c09c819af5d52b6b7f183
Alexander Stepanov <vaiol.ans@gmail.com>
2021-12-20 22:36:30 +0200

    1200

--- src/1200/Solution.py
+++ src/1200/Solution.py
@@ -6,3 +6,19 @@ class Solution:
         arr.sort()
         Min = min(m - n for n, m in zip(arr, arr[1:]))
         return [[n, m] for n, m in zip(arr, arr[1:]) if Min == m - n]
+
+    def minimumAbsDifference_2(self, arr: List[int]) -> List[List[int]]:
+        #20.12.2021
+        arr.sort()
+
+        res = list()
+        min_diff = float('inf')
+        for i in range(1, len(arr)):
+            diff = abs(arr[i - 1] - arr[i])
+            if diff < min_diff:
+                min_diff = diff
+                res = list()
+
+            if diff == min_diff:
+                res.append([arr[i - 1], arr[i]])
+        return res
