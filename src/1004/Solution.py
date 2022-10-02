a54f0f18865e7f42b24321af00e5b0c3faa4fdd7
Alexander Stepanov <vaiol.ans@gmail.com>
2022-10-02 16:00:51 +0200

    1004

---
+++ src/1004/Solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        max_len = 0
+        left, right = 0, 0
+        
+        curr_len = 0
+        curr_k = k
+        while right < len(nums):
+            while right < len(nums) and (nums[right] == 1 or curr_k > 0):
+                if nums[right] == 0:
+                    curr_k -= 1
+                right += 1
+            curr_len = right - left
+            max_len = max(max_len, curr_len)
+            while curr_k == 0 and left < len(nums):
+                if (nums[left] == 0):
+                    curr_k += 1
+                left += 1
+        return max_len
+            
+            
+                
+            
